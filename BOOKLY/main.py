35cebcef71c0ad1943029c72be7c2f5266da3aee
Ajay Dev <devadigaajay1729@gmail.com>
2025-01-31 19:20:07 +0530

    added return tyoe

--- BOOKLY/main.py
+++ BOOKLY/main.py
@@ -6,11 +6,11 @@ app = FastAPI()
 
 # define the roots
 @app.get('/')
-async def read_root():
+async def read_root() -> dict:
     return {"message": "Hello World"}
 
 
 # route to access the value in the url
 @app.get('/greet/{name}')
-async def greet_user(name: str):
+async def greet_user(name: str) -> dict:
     return {"message": f"Hello!, {name}"}
