95d0a6b258804d53ac287b8bf8299fd881cf333c
Ajay Dev <devadigaajay1729@gmail.com>
2025-01-31 17:20:40 +0530

    added simple route to greet

--- BOOKLY/main.py
+++ BOOKLY/main.py
@@ -1,10 +1,16 @@
-from fastapi import FASTAPI
+from fastapi import FastAPI
 
 # create the fastapi app
-app = FASTAPI()
+app = FastAPI()
 
 
 # define the roots
 @app.get('/')
 async def read_root():
     return {"message": "Hello World"}
+
+
+# route to access the value in the url
+@app.get('/greet/{name}')
+async def greet_user(name: str):
+    return {"message": f"Hello!, {name}"}
