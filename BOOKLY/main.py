72ab3a69e2802e55e460e7373b9adc46ff61b87c
Ajay Dev <devadigaajay1729@gmail.com>
2025-01-31 23:35:27 +0530

    query parameter example.

--- BOOKLY/main.py
+++ BOOKLY/main.py
@@ -14,3 +14,9 @@ async def read_root() -> dict:
 @app.get('/greet/{name}')
 async def greet_user(name: str) -> dict:
     return {"message": f"Hello!, {name}"}
+
+
+# query parameter example
+@app.get('/greetUser')
+async def greet_query_parameter_user(user: str) -> dict:
+    return {"message": f"Hello!, {user}"}
