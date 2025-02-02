26f01463db563b6ac0c6db5fe0f48839a40094af
Ajay Dev <devadigaajay1729@gmail.com>
2025-02-02 16:05:44 +0530

    added a delete method

--- FastAPI_examples/book.py
+++ FastAPI_examples/book.py
@@ -149,7 +149,7 @@ async def create_book(book_data: BookListingSerializer) -> dict:
 
 # update a book
 @app.patch('/books/{book_id}', response_model=BookListingSerializer, status_code=status.HTTP_200_OK)
-async def update_book(book_id: int, book_data: BookUpdateSerializer):
+async def update_book(book_id: int, book_data: BookUpdateSerializer) -> dict:
     for book in books:
         if book['id'] == book_id:
             book['title'] = book_data.title
@@ -159,5 +159,17 @@ async def update_book(book_id: int, book_data: BookUpdateSerializer):
             book['language'] = book_data.language
 
             return book
+
+    raise HTTPException(detail='Book not found', status_code=status.HTTP_404_NOT_FOUND)
+
+
+# delete a book
+@app.delete('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int) -> None:
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+
+            return {}
 
     raise HTTPException(detail='Book not found', status_code=status.HTTP_404_NOT_FOUND)
