d1bc8be2da8065f273cb669c8a463cbe82ec3ed9
clutch_god17 <devadigaajay1729@gmail.com>
2025-02-15 00:01:58 +0530

    added review related services and schemas

--- src/reviews/service.py
+++ src/reviews/service.py
@@ -1,5 +1,45 @@
 from src.db.models import Review
-from src.auth.services import UserService
+from src.auth.service import UserService
 from src.books.service import BookService
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from src.reviews.schemas import ReviewCreateModel
+from fastapi.exceptions import HTTPException
+from fastapi import status
 
-import Bookly.src
+# create a service to manage books and users
+user_service = UserService()
+book_service = BookService()
+
+
+class ReviewService:
+
+    async def add_review_to_book(
+        self,
+        user_email: str,
+        book_uid: str,
+        review_data: ReviewCreateModel,
+        session: AsyncSession,
+    ):
+        try:
+            book = await book_service.get_book(
+                book_uid=book_uid,
+                session=session
+            )
+
+            user = await user_service.get_user_by_email(
+                email=user_email,
+                session=session
+            )
+
+            review_dict = review_data.model_dump()
+            review = Review(
+                **review_dict
+            )
+
+            review.user = user
+            review.book = book
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Failed to add review to book: {str(e)}"
+            )
